fed0ddd9a596ad669c1e50c03af4aa4be5f1c8b8
IvanAziz <barbosax@gmail.com>
2021-03-23 01:27:51 -0300

    Atualização

---
+++ primeiro_projeto/lib/main_001_text_exemple.dart
@@ -0,0 +1,33 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    title: "Frases do dia",
+    home: Container(
+      color: Colors.white,
+      child: Column(
+          children: <Widget>[
+            Text(
+                "orem ipsum dolor sit amet, consectetur adipiscing elit.",
+                style: TextStyle(
+                  fontSize: 50,
+                  fontStyle: FontStyle.normal,
+                  fontWeight: FontWeight.normal,
+                  letterSpacing: 0,
+                  wordSpacing: 0,
+                  decoration: TextDecoration.combine(<TextDecoration>[
+                      TextDecoration.overline,
+                      TextDecoration.lineThrough]),
+                  decorationColor: Colors.greenAccent,
+                  decorationStyle: TextDecorationStyle.double,
+                  color: Colors.blue,
+                )
+            )
+          ]
+      )
+    )
+  ));
+}
+
---
+++ primeiro_projeto/lib/main_002_button_event_log_generate.dart
@@ -0,0 +1,18 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MaterialApp(
+    //debugShowCheckedModeBanner: false,
+    title: "Frases do dia",
+    home: Container(
+      color: Colors.white,
+      child: Column(
+          children: <Widget>[
+          ]
+      )
+    )
+  ));
+}
+
