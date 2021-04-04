19a9ff811d3b0eeb4a9bf850e772365f16613ab9
IvanAziz <barbosax@gmail.com>
2021-04-04 18:02:54 -0300

    App: Frases do dia

--- primeiro_projeto/lib/main.dart
+++ primeiro_projeto/lib/main.dart
@@ -1,65 +1,79 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-        title: "Frases do Dia",
-        home: Home()
-    );
-  }
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
 
-class Home extends StatelessWidget{
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  var _frases = [
+    "A única certeza é que a mudança é inevitável",
+    "O medo de perder tira a vontade de ganhar",
+    "A vida é finita, faça valer a pena",
+    "As melhores coisas da vida passam despercebido, fique atento e aproveite",
+    "Dê o primeiro passo e depois disso continue dando passos até chegar, ficar só observando e falando sobre onde deseja chegar não vai te levar lá. Realização depende de ação.",
+  ];
+  var _fraseGerada = "Clique abaixo para gerar uma frase!";
+
+  void _gerarFrase() {
+    var numeroSorteado = Random().nextInt(_frases.length);
+    setState(() {
+      _fraseGerada = _frases[numeroSorteado];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    String _titulo = "Frases do Dia";
-
     return Scaffold(
       appBar: AppBar(
-        title: Text(_titulo),
+        title: Text("Frases do dia"),
         backgroundColor: Colors.green,
       ),
-      body: Column(        
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-            Image.asset("images/logo.png"),
-            Padding(
-              padding: EdgeInsets.all(16),
-              child: Center(
-                child: Text(
-                  "Gratidão não é pagamento, mas um reconhecimento que se demostra no dia a dia.",
-                )
-              )
-            ),
-            TextButton(
-              onPressed: () => print("Nova Frase"), 
-              child: Text(
-                "Nova Frase",
+      body: Center(
+        child: Container(
+          padding: EdgeInsets.all(16),
+          //width: double.infinity,
+          /*decoration:
+              BoxDecoration(border: Border.all(width: 3, color: Colors.amber)),*/
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset("images/logo.png"),
+              Text(
+                _fraseGerada,
+                textAlign: TextAlign.justify,
                 style: TextStyle(
-                  color: Colors.white,
-                )
-                ),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateColor.resolveWith((states) => Colors.green),
+                    fontSize: 25,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.black),
               ),
-              )              
-        ]),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.green,
+                  onPrimary: Colors.white,
+                ),
+                child: Text(
+                  "Nova Frase",
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                ),
+                onPressed: _gerarFrase,
+              )
+            ],
+          ),
+        ),
+      ),
     );
-  }
-}
-
-class HomeStatefull extends StatefulWidget{
-  @override
-  State<StatefulWidget> createState() => _HomeStatefullState();
-}
-
-class _HomeStatefullState extends State<HomeStatefull>{
-  @override
-  Widget build(BuildContext context) {
-    return Container();
   }
 }
