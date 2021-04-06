68ece2415978abb3b9d8408603bfc26a9f29f7b5
IvanAziz <barbosax@gmail.com>
2021-04-05 22:12:36 -0300

    Game JokenPo

--- primeiro_projeto/lib/main.dart
+++ primeiro_projeto/lib/main.dart
@@ -1,79 +1,9 @@
-import 'dart:math';
 import 'package:flutter/material.dart';
+import 'Jokenpo.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: Home(),
-    debugShowCheckedModeBanner: false,
-  ));
-}
-
-class Home extends StatefulWidget {
-  @override
-  _HomeState createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  var _frases = [
-    "A única certeza é que a mudança é inevitável",
-    "O medo de perder tira a vontade de ganhar",
-    "A vida é finita, faça valer a pena",
-    "As melhores coisas da vida passam despercebido, fique atento e aproveite",
-    "Dê o primeiro passo e depois disso continue dando passos até chegar, ficar só observando e falando sobre onde deseja chegar não vai te levar lá. Realização depende de ação.",
-  ];
-  var _fraseGerada = "Clique abaixo para gerar uma frase!";
-
-  void _gerarFrase() {
-    var numeroSorteado = Random().nextInt(_frases.length);
-    setState(() {
-      _fraseGerada = _frases[numeroSorteado];
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Frases do dia"),
-        backgroundColor: Colors.green,
-      ),
-      body: Center(
-        child: Container(
-          padding: EdgeInsets.all(16),
-          //width: double.infinity,
-          /*decoration:
-              BoxDecoration(border: Border.all(width: 3, color: Colors.amber)),*/
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Image.asset("images/logo.png"),
-              Text(
-                _fraseGerada,
-                textAlign: TextAlign.justify,
-                style: TextStyle(
-                    fontSize: 25,
-                    fontStyle: FontStyle.italic,
-                    color: Colors.black),
-              ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.green,
-                  onPrimary: Colors.white,
-                ),
-                child: Text(
-                  "Nova Frase",
-                  style: TextStyle(
-                      fontSize: 25,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold),
-                ),
-                onPressed: _gerarFrase,
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
+void main(){
+    runApp(MaterialApp(
+     home: Jokenpo(),
+     debugShowCheckedModeBanner: false
+    ));
 }
