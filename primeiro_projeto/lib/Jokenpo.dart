e22744f933df30fa073fdc2279085df7ed850bcf
IvanAziz <barbosax@gmail.com>
2021-04-06 01:39:40 -0300

    Desafio:   - Inclusão de contador de:      - Partida ganha      - Partida empatada      - Partida perdida   - Inclusão de opção para reiniciar o jogo voltando os contadores, mensagens e imagens ao estado inicial.

--- primeiro_projeto/lib/Jokenpo.dart
+++ primeiro_projeto/lib/Jokenpo.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:math';
 
@@ -9,6 +10,20 @@ class Jokenpo extends StatefulWidget {
 class _JokenpoState extends State<Jokenpo> {
   var _imagemApp = AssetImage("images/padrao.png");
   var _mensagem = "Escolha uma opção abaixo";
+  var _vezesGanhou = 0;
+  var _vezesPerdeu = 0;
+  var _vezesEmpate = 0;
+
+  void _reiniciaContador()
+  {
+    setState(() {
+      this._imagemApp = AssetImage("images/padrao.png");
+      this._mensagem = "Escolha uma opção abaixo";
+      this._vezesEmpate = 0;
+      this._vezesGanhou = 0;
+      this._vezesPerdeu = 0;
+    });
+  }
 
   void _opcaoSelecionada(String escolhaUsuario) {
     var _opcoes = ["pedra", "papel", "tesoura"];
@@ -40,16 +55,19 @@ class _JokenpoState extends State<Jokenpo> {
         escolhaUsuario == "papel" && escolhaApp == "pedra") {
       setState(() {
         this._mensagem = "Parabéns!!! Você ganhou :)";
+        this._vezesGanhou++;
       });
     } else if (escolhaApp == "pedra" && escolhaUsuario == "tesoura" ||
         escolhaApp == "tesoura" && escolhaUsuario == "papel" ||
         escolhaApp == "papel" && escolhaUsuario == "pedra") {
       setState(() {
         this._mensagem = "Você perdeu :(";
+        this._vezesPerdeu++;
       });
     } else {
       setState(() {
         this._mensagem = "Empatamos :|";
+        this._vezesEmpate++;
       });
     }
   }
@@ -57,39 +75,70 @@ class _JokenpoState extends State<Jokenpo> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("JokenPo")),
-      body: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-                padding: EdgeInsets.only(top: 32, bottom: 16),
-                child: Text("Escolha do App:",
-                    textAlign: TextAlign.center,
-                    style:
-                        TextStyle(fontSize: 20, fontWeight: FontWeight.bold))),
-            Image(image: this._imagemApp),
-            Padding(
-                padding: EdgeInsets.only(top: 32, bottom: 16),
-                child: Text(_mensagem,
-                    textAlign: TextAlign.center,
-                    style:
-                        TextStyle(fontSize: 20, fontWeight: FontWeight.bold))),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
+        appBar: AppBar(title: Text("JokenPo")),
+        body: Column(crossAxisAlignment: CrossAxisAlignment.center, children: <
+            Widget>[
+          Padding(
+              padding: EdgeInsets.only(top: 32, bottom: 16),
+              child: Text("Escolha do App:",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))),
+          Image(image: this._imagemApp),
+          Padding(
+              padding: EdgeInsets.only(top: 32, bottom: 16),
+              child: Text(_mensagem,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold))),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              GestureDetector(
+                  onTap: () => _opcaoSelecionada("pedra"),
+                  child: Image.asset("images/pedra.png", height: 100)),
+              GestureDetector(
+                  onTap: () => _opcaoSelecionada("papel"),
+                  child: Image.asset("images/papel.png", height: 100)),
+              GestureDetector(
+                  onTap: () => _opcaoSelecionada("tesoura"),
+                  child: Image.asset("images/tesoura.png", height: 100)),
+            ],
+          ),
+          GestureDetector(
+            onTap: () => {
+              _reiniciaContador(),
+            },
+            child: Padding(
+              padding: EdgeInsets.only(top: 160),
+              child: Text(
+                "Clique para reiniciar partida",
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+              ),
+            )
+          )
+        ]),
+        bottomNavigationBar: BottomAppBar(
+          color: Colors.blue,
+          shape: const CircularNotchedRectangle(),
+          child: Container(
+            height: 50.0,
+            child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: <Widget>[
-                GestureDetector(
-                    onTap: () => _opcaoSelecionada("pedra"),
-                    child: Image.asset("images/pedra.png", height: 100)),
-                GestureDetector(
-                    onTap: () => _opcaoSelecionada("papel"),
-                    child: Image.asset("images/papel.png", height: 100)),
-                GestureDetector(
-                    onTap: () => _opcaoSelecionada("tesoura"),
-                    child: Image.asset("images/tesoura.png", height: 100))
+                Text(
+                  ":) = ${this._vezesGanhou}",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  ":| = ${this._vezesEmpate}",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                Text(":( = ${this._vezesPerdeu}",
+                    style:
+                        TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
               ],
-            )
-          ]),
-    );
+            ),
+          ),
+        ));
   }
 }
