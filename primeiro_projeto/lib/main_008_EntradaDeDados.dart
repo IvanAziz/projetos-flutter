4cc5881607f92fe6c8d7db438d039ec30b1d6bcb
IvanAziz <barbosax@gmail.com>
2021-04-27 12:55:41 -0300

    Renomeado main.dart para main_008_EntradaDeDados.dart

---
+++ primeiro_projeto/lib/main_008_EntradaDeDados.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:primeiro_projeto/EntradaDados/EntradaSlider.dart';
+
+///import 'package:primeiro_projeto/EntradaDados/EntradaSwitch.dart';
+///import 'package:primeiro_projeto/EntradaDados/EntradaRadioButton.dart';
+///import 'package:primeiro_projeto/EntradaDados/EntradaCheckBox.dart';
+///import 'package:primeiro_projeto/CampoTexto.dart';
+
+// Entrada de dados, alterar o home para validar diferentes entradas de dados
+void main() {
+  runApp(
+    MaterialApp(
+      //home: CampoTexto(),
+      //home: EntradaCheckBox(),
+      //home: EntradaRadioButton(),
+      //home: EntradaSwitch(),
+      home: EntradaSlider(),
+    ),
+  );
+}
