4ca8b87af5cb863524fad35cce304cf6d50b79da
IvanAziz <barbosax@gmail.com>
2021-04-28 21:43:08 -0300

    App 0010 navegação entre múltiplas telas gesto em imagem.

--- primeiro_projeto/lib/main.dart
+++ primeiro_projeto/lib/main.dart
@@ -1,12 +1,100 @@
 import 'package:flutter/material.dart';
 
-import 'AtmConsultoria/HomeApp010.dart';
-
 void main() {
   runApp(
     MaterialApp(
-      home: HomeApp010(),
-      debugShowCheckedModeBanner: false,
+      home: TelaPrincipal(),
     ),
   );
+}
+
+class TelaPrincipal extends StatefulWidget {
+  @override
+  _TelaPrincipalState createState() => _TelaPrincipalState();
+}
+
+class _TelaPrincipalState extends State<TelaPrincipal> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tela principal"),
+        backgroundColor: Colors.orange,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(32),
+        child: Column(
+          children: <Widget>[
+            ElevatedButton(
+              style: ButtonStyle(
+                padding: MaterialStateProperty.resolveWith(
+                        (states) => EdgeInsets.all(15)),
+              ),
+              child: Text("Ir para a segunda tela"),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => TelaSecundaria()));
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TelaSecundaria extends StatefulWidget {
+  @override
+  _TelaSecundariaState createState() => _TelaSecundariaState();
+}
+
+class _TelaSecundariaState extends State<TelaSecundaria> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tela Secundária"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(32),
+        child: Column(
+          children: <Widget>[
+            ElevatedButton(
+              style: ButtonStyle(
+                padding: MaterialStateProperty.resolveWith(
+                        (states) => EdgeInsets.all(15)),
+              ),
+              child: Text("Ir para a terceira tela"),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => TelaTerceira()));
+              },
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TelaTerceira extends StatefulWidget {
+  @override
+  _TelaTerceiraState createState() => _TelaTerceiraState();
+}
+
+class _TelaTerceiraState extends State<TelaTerceira> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Tela Terceira"),
+        backgroundColor: Colors.pink,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(32),
+        child: Text("Chegou na terceira tela"),
+      ),
+    );
+  }
 }
---
+++ primeiro_projeto/lib/main_010_app_NavegacaoPorImagem.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'AtmConsultoria/HomeApp010.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: HomeApp010(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
